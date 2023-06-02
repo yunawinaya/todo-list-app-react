9e95dea442bff8969f148a9db145e654c4d346ac
yunawinaya <gedeyuna@gmail.com>
2023-06-02 09:46:00 +0700

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,54 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import { useState } from "react";
+import Button from "react-bootstrap/Button";
+
+function App() {
+  const [todos, setTodos] = useState([]);
+  const [newTodo, setNewTodo] = useState("");
+
+  function addTodo() {
+    setTodos([...todos, newTodo]);
+    setNewTodo("");
+  }
+
+  function deleteTodo(index) {
+    let newTodos = [...todos];
+    newTodos.splice(index, 1);
+    setTodos(newTodos);
+  }
+
+  function ToDoList({ todos }) {
+    return (
+      <ul>
+        {todos.map((todo, index) => (
+          <li key={index}>
+            {todo}
+            <Button
+              variant="danger"
+              onClick={() => deleteTodo(index)}
+              className="ms-3"
+            >
+              Delete
+            </Button>
+          </li>
+        ))}
+      </ul>
+    );
+  }
+
+  return (
+    <div>
+      <input
+        type="text"
+        value={newTodo}
+        onChange={(event) => setNewTodo(event.target.value)}
+      />
+      <Button variant="primary" onClick={addTodo} className="ms-3">
+        Add
+      </Button>
+      <ToDoList todos={todos} />
+    </div>
+  );
+}
+
+export default App;
